668f23e941faf85a793acea8f15d99a5fe64080d
SGoriainov <wishmast777@gmail.com>
2023-09-09 20:33:53 +0400

    exception custom test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,26 +3,26 @@ package org.example;
 import java.util.*;
 
 // Создаем собственное исключение
-class InvalidEmailException extends Exception {
-    public InvalidEmailException(String message) {
+class CustomException extends Exception {
+    public CustomException(String message) {
         super(message);
     }
 }
 
 // Класс, который использует собственное исключение
-class EmailValidator {
+class Example {
     // Метод, который может выбросить собственное исключение
-    public static void validateEmail(String email) throws InvalidEmailException {
-        if (!email.contains("@")) {
-            throw new InvalidEmailException("Некорректный адрес электронной почты");
+    public static void doSomething(int value) throws CustomException {
+        if (value < 0) {
+            throw new CustomException("Значение должно быть больше или равно нулю");
         }
     }
 
     public static void main(String[] args) {
         try {
             // Вызываем метод, который может выбросить исключение
-            validateEmail("example.com");
-        } catch (InvalidEmailException e) {
+            doSomething(5);
+        } catch (CustomException e) {
             // Обрабатываем исключение
             System.out.println("Ошибка: " + e.getMessage());
         }
