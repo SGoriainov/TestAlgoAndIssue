6c5b2a09dd1da00449d77ec997b0ceffd260ae85
SGoriainov <wishmast777@gmail.com>
2023-09-09 20:28:21 +0400

    exception custom

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,46 +2,29 @@ package org.example;
 
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-//        if (word_a[i] == word_b[1]) {
-//            cell[i][j] = cell[i - 1][j - 1] + 1;
-//        } else {
-//            cell[i][j] = Math.Max(cell[i - 1][j], cell[i][j - 1]);
-//        }
-            String wordA = "blue";
-            String wordB = "clue";
-            int[][] cell = new int[wordA.length()][wordB.length()];
+// Создаем собственное исключение
+class InvalidEmailException extends Exception {
+    public InvalidEmailException(String message) {
+        super(message);
+    }
+}
 
-            for (int i = 0; i < wordA.length(); i++) {
-                for (int j = 0; j < wordB.length(); j++) {
-                    // The letters match
-                    if (wordA.charAt(i) == wordB.charAt(j)) {
-                        if (i > 0 && j > 0) {
-                            cell[i][j] = cell[i - 1][j - 1] + 1;
-                        } else {
-                            cell[i][j] = 1;
-                        }
-                    } else {
-                        // The letters don't match.
-                        if (i > 0 && j > 0) {
-                            cell[i][j] = Math.max(cell[i - 1][j], cell[i][j - 1]);
-                        } else {
-                            cell[i][j] = 0;
-                        }
-                    }
-                }
-            }
-
-            printResult(cell);
-//      [0, 0, 0, 1]
-//      [0, 1, 1, 1]
-//      [0, 1, 2, 2]
-//      [0, 1, 2, 3]
-        }
-        private static void printResult(int[][] arr) {
-            for (int[] row : arr) {
-                System.out.println(Arrays.toString(row));
-            }
+// Класс, который использует собственное исключение
+class EmailValidator {
+    // Метод, который может выбросить собственное исключение
+    public static void validateEmail(String email) throws InvalidEmailException {
+        if (!email.contains("@")) {
+            throw new InvalidEmailException("Некорректный адрес электронной почты");
         }
     }
+
+    public static void main(String[] args) {
+        try {
+            // Вызываем метод, который может выбросить исключение
+            validateEmail("example.com");
+        } catch (InvalidEmailException e) {
+            // Обрабатываем исключение
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+}
