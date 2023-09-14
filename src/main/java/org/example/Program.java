7a3a3a3f77e7ab7150a4d6606d54a34063b7651d
SGoriainov <wishmast777@gmail.com>
2023-09-14 14:30:53 +0400

    Philosopher issue

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -2,70 +2,55 @@ package org.example;
 
 import java.util.Scanner;
 
+import java.util.concurrent.Semaphore;
+
 public class Program {
 
     public static void main(String[] args) {
 
-        Store store=new Store();
-        Producer producer = new Producer(store);
-        Consumer consumer = new Consumer(store);
-        new Thread(producer).start();
-        new Thread(consumer).start();
+        Semaphore sem = new Semaphore(2);
+        for(int i=1;i<6;i++)
+            new Philosopher(sem,i).start();
     }
 }
-// Класс Магазин, хранящий произведенные товары
-class Store{
-    private int product=0;
-    public synchronized void get() {
-        while (product<1) {
-            try {
-                wait();
-            }
-            catch (InterruptedException e) {
-            }
-        }
-        product--;
-        System.out.println("Покупатель купил 1 товар");
-        System.out.println("Товаров на складе: " + product);
-        notify();
+// класс философа
+class Philosopher extends Thread
+{
+    Semaphore sem; // семафор. ограничивающий число философов
+    // кол-во приемов пищи
+    int num = 0;
+    // условный номер философа
+    int id;
+    // в качестве параметров конструктора передаем идентификатор философа и семафор
+    Philosopher(Semaphore sem, int id)
+    {
+        this.sem=sem;
+        this.id=id;
     }
-    public synchronized void put() {
-        while (product>=3) {
-            try {
-                wait();
-            }
-            catch (InterruptedException e) {
-            }
-        }
-        product++;
-        System.out.println("Производитель добавил 1 товар");
-        System.out.println("Товаров на складе: " + product);
-        notify();
-    }
-}
-// класс Производитель
-class Producer implements Runnable{
 
-    Store store;
-    Producer(Store store){
-        this.store=store;
-    }
-    public void run(){
-        for (int i = 1; i < 6; i++) {
-            store.put();
-        }
-    }
-}
-// Класс Потребитель
-class Consumer implements Runnable{
+    public void run()
+    {
+        try
+        {
+            while(num<3)// пока количество приемов пищи не достигнет 3
+            {
+                //Запрашиваем у семафора разрешение на выполнение
+                sem.acquire();
+                System.out.println ("Философ " + id+" садится за стол");
+                // философ ест
+                sleep(500);
+                num++;
 
-    Store store;
-    Consumer(Store store){
-        this.store=store;
-    }
-    public void run(){
-        for (int i = 1; i < 6; i++) {
-            store.get();
+                System.out.println ("Философ " + id+" выходит из-за стола");
+                sem.release();
+
+                // философ гуляет
+                sleep(500);
+            }
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println ("у философа " + id + " проблемы со здоровьем");
         }
     }
 }
