14cbca90c275aeee357bdbc4aa1dfca867d9d95e
SGoriainov <wishmast777@gmail.com>
2023-09-25 12:39:27 +0400

    238. Product of Array Except Self (n^2)

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -7,11 +7,17 @@ public class Program {
 //217. Contains Duplicate
     public static void main(String[] args) {
 
-        int[] nums = new int[]{1, 2, 3, 4};
-        Set<Integer> set = new HashSet<>();
+        int[] nums = new int[]{1, 2, 3, 5};
+        int n = nums.length;
+        int[] result = new int[n];
 
-        for (int num : nums) {
-            set.add(num);
+        for (int i = 0; i < n; i++) {
+            int sum = 1;
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                sum = sum * nums[j];
+            }
+            result[i] = sum;
         }
 
     }
