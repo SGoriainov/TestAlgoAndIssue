92f282908b70e504143dc74093a33b73359688a6
SGoriainov <wishmast777@gmail.com>
2023-09-29 14:51:01 +0400

    153. Find Minimum in Rotated Sorted Array

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -4,23 +4,14 @@ import java.util.*;
 
 
 public class Program {
-//152. Maximum Product Subarray
+//153. Find Minimum in Rotated Sorted Array
     public static void main(String[] args) {
 
-        int[] nums = new int[]{-2,0,-1};
+        int[] nums = new int[]{3,4,5,1,2};
 
-        int min = nums[0];
-        int max = nums[0];
-        int res=nums[0];
+        for( int i=1; i< nums.length; i++) {
+            if(nums[i]<= nums[i-1]) System.out.println(nums[i]);
 
-        for (int i=1; i<nums.length; i++) {
-
-            int temp = max * nums[i];
-
-            max = Integer.max(nums[i],(Integer.max(nums[i]*min, nums[i]*max)));
-            min = Integer.min(nums[i],(Integer.min(nums[i]*min, temp)));
-            res = Integer.max(res,max);
         }
-        System.out.println(res);
     }
 }
