3b151e721de03710cd831435b8a48230ac514eb4
SGoriainov <wishmast777@gmail.com>
2023-10-02 13:49:40 +0400

    33. Search in Rotated Sorted Array

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -4,20 +4,31 @@ import java.util.*;
 
 
 public class Program {
-//153. Find Minimum in Rotated Sorted Array
+//33. Search in Rotated Sorted Array
     public static void main(String[] args) {
 
-        int[] nums = new int[]{3,4,5,1,2};
+        int[] nums = new int[]{4,5,6,7,0,1,2};
+        int target =3 ;
 
         int low = 0;
-        int high = nums.length - 1;
-        while(low < high){
-            int mid = (low + high) / 2;
-            if(nums[high] < nums[mid]){
-                low = mid + 1;
-            } else {
-                high = mid;
+        int high = nums.length-1;
+
+        while(low<=high) {
+            int mid = low+(high-low) /2;
+
+            if(nums[mid]==target) {
+                System.out.println(mid);
+                break;
             }
+            if(nums[low] <= nums[mid]) {
+                if(target >= nums[low] && target <nums[mid]) high = mid-1;
+                else low = mid+1;
+            }
+            if(nums[high] >= nums[mid]) {
+                if (target <= nums[high] && target > nums[mid]) low = mid+1;
+                else high = mid-1;
+            }
+            System.out.println("mid =" +mid +"   low= "+low+"   high="+high);
         }
 
     }
