dae8fe240f298753c6888627e3a6e45a1284db32
SGoriainov <wishmast777@gmail.com>
2023-10-12 21:53:12 +0400

    1. Two Sum_2

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -4,21 +4,20 @@ import java.util.*;
 
 
 public class Program {
-//33. Search in Rotated Sorted Array
+//1. Two Sum
     public static void main(String[] args) {
 
         int[] nums = new int[]{4,5,6,7,0,1,2};
         int target =3 ;
 
 
-        int n = nums.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i]))
+                return new int[] {map.get(target - nums[i]) + 1, i + 1};
+            else map.put(nums[i], i);
         }
-        return new int[]{}; // No solution found
+        return null;
+
     }
 }
