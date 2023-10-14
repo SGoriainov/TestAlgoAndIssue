46535daad6e8daf357566b8df10ec13607e76947
SGoriainov <wishmast777@gmail.com>
2023-10-14 21:48:05 +0400

    1. Two Sum_4

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -11,17 +11,10 @@ public class Program {
         int target =3 ;
 
 
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int[] eresult = {0,0};
-
-        for(int i = 0; i < nums.length; i++) {
-            if(map.get(nums[i]) != null) {
-                int[] result = {map.get(nums[i])+1,i+1};
-                return result;
-            }
-            map.put(target-nums[i], i);
-        }
-        return eresult;
-
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; map.put(nums[i], i++))
+            if (map.containsKey(target - nums[i]))
+                return new int[] {map.get(target - nums[i]), i};
+        return new int[] {0, 0};
     }
 }
