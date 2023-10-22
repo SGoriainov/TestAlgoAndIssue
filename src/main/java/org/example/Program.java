8ba228ba67b9a9f8b4c91544f0d0884e9e4bbcea
SGoriainov <wishmast777@gmail.com>
2023-10-22 18:27:54 +0400

    322. Coin Change

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -7,29 +7,25 @@ public class Program {
 //1. Two Sum
     public static void main(String[] args) {
 
-        public boolean wordBreak(String s, Set<String> dict) {
-            // DFS
-            Set<Integer> set = new HashSet<Integer>();
-            return dfs(s, 0, dict, set);
+        public int coinChange(int[] coins, int amount) {
+            if(amount<1) return 0;
+            return helper(coins, amount, new int[amount]);
         }
 
-        private boolean dfs(String s, int index, Set<String> dict, Set<Integer> set){
-            // base case
-            if(index == s.length()) return true;
-            // check memory
-            if(set.contains(index)) return false;
-            // recursion
-            for(int i = index+1;i <= s.length();i++){
-                String t = s.substring(index, i);
-                if(dict.contains(t))
-                    if(dfs(s, i, dict, set))
-                        return true;
-                    else
-                        set.add(i);
+        private int helper(int[] coins, int rem, int[] count) { // rem: remaining coins after the last step; count[rem]: minimum number of coins to sum up to rem
+            if(rem<0) return -1; // not valid
+            if(rem==0) return 0; // completed
+            if(count[rem-1] != 0) return count[rem-1]; // already computed, so reuse
+            int min = Integer.MAX_VALUE;
+            for(int coin : coins) {
+                int res = helper(coins, rem-coin, count);
+                if(res>=0 && res < min)
+                    min = 1+res;
             }
-            set.add(index);
-            return false;
+            count[rem-1] = (min==Integer.MAX_VALUE) ? -1 : min;
+            return count[rem-1];
         }
+
 
     }
 }
