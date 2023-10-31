f1358da9871b5d0e4ba7fdaf8b76faa53f4a6215
SGoriainov <wishmast777@gmail.com>
2023-10-31 20:22:11 +0400

    322. Coin Change

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -7,25 +7,10 @@ public class Program {
 //1. Two Sum
     public static void main(String[] args) {
 
-        public int coinChange(int[] coins, int amount) {
-            if(amount<1) return 0;
-            return helper(coins, amount, new int[amount]);
+        public int[] countBits ( int num){
+            int[] f = new int[num + 1];
+            for (int i = 1; i <= num; i++) f[i] = f[i >> 1] + (i & 1);
+            return f;
         }
-
-        private int helper(int[] coins, int rem, int[] count) { // rem: remaining coins after the last step; count[rem]: minimum number of coins to sum up to rem
-            if(rem<0) return -1; // not valid
-            if(rem==0) return 0; // completed
-            if(count[rem-1] != 0) return count[rem-1]; // already computed, so reuse
-            int min = Integer.MAX_VALUE;
-            for(int coin : coins) {
-                int res = helper(coins, rem-coin, count);
-                if(res>=0 && res < min)
-                    min = 1+res;
-            }
-            count[rem-1] = (min==Integer.MAX_VALUE) ? -1 : min;
-            return count[rem-1];
-        }
-
-
     }
 }
