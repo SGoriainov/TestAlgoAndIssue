683ed8e51ca9f8d216a9915f5c596507fcb3c805
SGoriainov <wishmast777@gmail.com>
2023-11-04 22:29:55 +0400

    207. Course Schedule

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -5,18 +5,39 @@ import java.util.*;
 
 public class Program {
 //1. Two Sum
-public int maxArea(int[] height) {
-    int left = 0, right = height.length - 1;
-    int maxArea = 0;
+public boolean canFinish(int numCourses, int[][] prerequisites) {
+    ArrayList[] graph = new ArrayList[numCourses];
+    int[] degree = new int[numCourses];
+    Queue queue = new LinkedList();
+    int count=0;
 
-    while (left < right) {
-        maxArea = Math.max(maxArea, Math.min(height[left], height[right])
-                * (right - left));
-        if (height[left] < height[right])
-            left++;
-        else
-            right--;
+    for(int i=0;i<numCourses;i++)
+        graph[i] = new ArrayList();
+
+    for(int i=0; i<prerequisites.length;i++){
+        degree[prerequisites[i][1]]++;
+        graph[prerequisites[i][0]].add(prerequisites[i][1]);
+    }
+    for(int i=0; i<degree.length;i++){
+        if(degree[i] == 0){
+            queue.add(i);
+            count++;
+        }
     }
 
-    return maxArea;
-}
+    while(queue.size() != 0){
+        int course = (int)queue.poll();
+        for(int i=0; i<graph[course].size();i++){
+            int pointer = (int)graph[course].get(i);
+            degree[pointer]--;
+            if(degree[pointer] == 0){
+                queue.add(pointer);
+                count++;
+            }
+        }
+    }
+    if(count == numCourses)
+        return true;
+    else
+        return false;
+}}
