e37c7125c14ee8136c451c8b95241274d94bf8f7
SGoriainov <wishmast777@gmail.com>
2023-11-19 20:09:32 +0400

    trying to testing

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.*;
+
+
+public class Main {
+//1. Two Sum
+    public static void main(String[] args) {
+        System.out.println("i'm working inside docker");
+    }
+}
