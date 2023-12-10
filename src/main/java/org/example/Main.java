4f7729165cc620db1ea640dc9878f6d892d791ce
SGoriainov <wishmast777@gmail.com>
2023-12-10 12:12:37 +0400

    two pointer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,32 @@ import java.util.*;
 
 
 public class Main {
-//1. Two Sum
+    //42. Trapping Rain Water
     public static void main(String[] args) {
-        System.out.println("i'm working inside docker");
+        class Solution {
+            public int trap(int[] height) {
+                int left = 0, right = height.length - 1;
+                int leftMax = height[0], rightMax = height[height.length - 1];
+                int water = 0;
+                while (left < right) {
+                    if (leftMax < rightMax) {
+                        left++;
+                        if (leftMax < height[left]) {
+                            leftMax = height[left];
+                        } else {
+                            water += leftMax - height[left];
+                        }
+                    } else {
+                        right--;
+                        if (rightMax < height[right]) {
+                            rightMax = height[right];
+                        } else {
+                            water += rightMax - height[right];
+                        }
+                    }
+                }
+                return water;
+            }
+        }
     }
 }
