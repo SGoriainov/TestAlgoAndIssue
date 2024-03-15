63cd1d801642f69b4afd637ceb2f1c244e7e705f
SGoriainov <wishmast777@gmail.com>
2024-03-15 10:11:01 +0400

    1108. Defanging an IP Address

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -4,39 +4,17 @@ import java.util.*;
 
 
 public class Solution {
-    public static int finalValueAfterOperations(String[] operations) {
-        int x =0;
-        for (int i=0; i<operations.length; i++) {
-            //--X
-            if (operations[i].charAt(0)=='-') {
-                --x;
-            }
+    public static String defangIPaddr(String address) {
 
-            //X--
-            if (operations[i].charAt(2)=='-') {
-                --x;
-            }
-
-            //++X
-            if (operations[i].charAt(0)=='+') {
-                ++x;
-            }
-
-            //X++
-            if (operations[i].charAt(2)=='+') {
-                x++;
-            }
-        }
-        return x;
+        String newAdress = address.replaceAll("\\.", "[,]");
+    return newAdress;
     }
 
     public static void main(String[] args) {
-        int res = 0;
-        String[] operations = new String[]
-                {"--X","X++","X++"};
 
-        res = finalValueAfterOperations(operations);
-        System.out.println(res);
+        String operations = "1.1.1.1";
+
+        System.out.println(defangIPaddr(operations));
 
     }
 }
