68bd6c504c028b5a044326ea0bfcf857407d4fa9
SGoriainov <wishmast777@gmail.com>
2024-03-18 14:11:57 +0400

    castom parseInt

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -5,21 +5,23 @@ import java.util.stream.IntStream;
 
 
 public class Solution {
-    public static int[] buildArray(int[] nums) {
-        int[] newArray = new int[nums.length];
-        for (int i = 0; i <nums.length; i++) {
-            newArray[i]= nums[nums[i]];
+    public static int buildArray(String string) {
+        char ch;
+        int res = 0;
+        for (int i = 0; i < string.length(); i++) {
 
+            res = res * 10 + (string.charAt(i) - '0');
         }
-        return newArray;
+        return res;
     }
 
     public static void main(String[] args) {
 
+        String st = "233";
         //char operations = 'e';
-       int[] operations2 = new int[] {0,2,1,5,3,4};
+        //String[] operations2 = new int[] {"0,2,1,5,3,4"};
 
-        System.out.println( buildArray(operations2));
+        System.out.println(buildArray(st));
 
     }
 }
