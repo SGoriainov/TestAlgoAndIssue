e35607ee2565a88b3902cd0cc81657dc04641577
SGoriainov <wishmast777@gmail.com>
2024-03-20 15:49:30 +0400

    2089. Find Target Indices After Sorting Array

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -5,21 +5,14 @@ import java.util.stream.IntStream;
 
 
 public class Solution {
-    public static int countNegatives(int[][] grid) {
-        int[] singleArr = Arrays.stream(grid)
-                .flatMapToInt(Arrays::stream)
-                .sorted()
-                .toArray();
-
-        int res = singleArr.length;
-
-        for (int i = 0; i < singleArr.length; i++) {
-            if (singleArr[i] >= 0) {
-                res = i;
-                break;
+    public static List<Integer> targetIndices(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==target) {
+                res.add(i);
             }
         }
-
         return res;
 
     }
@@ -29,9 +22,11 @@ public class Solution {
         //String st = "233";
         //char operations = 'e';
         //String[] operations2 = new int[] {"0,2,1,5,3,4"};
-        int[][] nums = new int[][]{{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}};
+        int target = 2;
+        int[] operations2 = new int[] {1,2,5,2,3};
+        //int[][] nums = new int[][]{{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}};
 
-        System.out.println(countNegatives(nums));
+        System.out.println(targetIndices(operations2,target));
 
     }
 }
