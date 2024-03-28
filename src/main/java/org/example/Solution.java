5ccf14da9d8fb1791138d8afa5ce685ff29ff412
SGoriainov <wishmast777@gmail.com>
2024-03-28 12:14:18 +0400

    1 задача. пересечение множеств.hashMap

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -20,36 +20,39 @@ public class Solution {
 
         //System.out.println(targetIndices(operations2,target));
 
-            int[] arr1 = {1, 2, 3, 2, 0};
-            int[] arr2 = {5, 1, 2, 7, 3, 2};
+        int[] arr1 = {1, 2, 3, 2, 0};
+        int[] arr2 = {5, 1, 2, 7, 3, 2};
 
-            int[] result = findIntersection(arr1, arr2);
-            System.out.println(Arrays.toString(result));
-        }
-
-    public static int[] findIntersection(int[] arr1, int[] arr2) {
-        int[] intersection = new int[Math.min(arr1.length, arr2.length)];
-        int intersectionIndex = 0;
-
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
-        int i = 0, j = 0;
-
-        while (i < arr1.length && j < arr2.length) {
-            if (arr1[i] == arr2[j]) {
-                intersection[intersectionIndex++] = arr1[i];
-                i++;
-                j++;
-            } else if (arr1[i] < arr2[j]) {
-                i++;
-            } else {
-                j++;
-            }
-        }
-
-        return Arrays.copyOf(intersection, intersectionIndex);
+        int[] result = findIntersection(arr1, arr2);
+        for (int num : result) {
+            System.out.print(num + " ");
         }
     }
 
+    public static int[] findIntersection(int[] arr1, int[] arr2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> intersection = new ArrayList<>();
+
+        // Заполнение хэш-таблицы уникальными элементами из первого массива
+        for (int num : arr1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Поиск элементов из второго массива в хэш-таблице
+        for (int num : arr2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                intersection.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+
+        // Преобразование списка в массив
+        int[] result = new int[intersection.size()];
+        for (int i = 0; i < intersection.size(); i++) {
+            result[i] = intersection.get(i);
+        }
+
+        return result;
+    }
+}
 
