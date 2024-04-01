b45260d5e4bc5ffb7d43bd9303ba3fa3e03d76f8
SGoriainov <wishmast777@gmail.com>
2024-04-01 12:06:49 +0400

    4 задача.  Максимальная длина подинтервала единиц с удалением одного элемента. Решение с двумя указателями

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -7,38 +7,44 @@ import java.util.stream.IntStream;
 
 public class Solution {
 
-    public static String setToGroup(Set<Integer> set) {
-        List<Integer> list = new ArrayList<>(set);
-        Collections.sort(list);
-        list.add(list.get(list.size() - 1));
-        System.out.println(list);
-        // "0-5,8-9,11"
+    public static int maxOnesLength(int[] nums) {
+        int maxOnesLength = 0;
+        int startOnesIndex = 0;
+        int onesCounter =0;
+        int zerosCounter = 0;
+        int secondOnesIndex =0;
 
-        StringBuilder result = new StringBuilder(String.valueOf(list.get(0)));
-        int startGroup = 0;
-        int endGroup = 0;
-        int count = 0;
-        for (int i = 0; i < list.size() - 1; i++) {
-            startGroup = list.get(i + 1);
-            endGroup = list.get(i);
-            if ((startGroup > endGroup + 1) && (count == 1)) {
-                result.append("," + startGroup);
-                count = 0;
-            } else if (list.get(i + 1) > list.get(i) + 1) {
-                result.append("-" + endGroup + "," + startGroup);
-                count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if( nums[i] ==1 && startOnesIndex ==0) { //если у нас начало последовательности единиц
+                startOnesIndex = i;
+                onesCounter++;
+            } else if (nums[i]==1) { //считаем последоватьность единиц
+                onesCounter++;
+            } else if (nums[i] == 0 && startOnesIndex !=0) {//если мы встретили 0 в последоватльности
+                zerosCounter++;
+                if (zerosCounter==2) {//если в последовательности единиц мы встречаем второй ноль
+                    zerosCounter--;
+                    maxOnesLength = Math.max(maxOnesLength,onesCounter+1);
+                    startOnesIndex =secondOnesIndex;
+                    onesCounter = nums[i]-secondOnesIndex;
+                    secondOnesIndex=0;
+                }
+                //если после 0 в последовательности идет 1, то запоминаем индекс второй последовательности
+            } else if (nums[i]==1 && zerosCounter ==1 && secondOnesIndex ==0) {
+                secondOnesIndex = i;
+            } else if (nums[i]==0) { //если идут нули
+                startOnesIndex =0;
+                zerosCounter =0;
+                secondOnesIndex =0;
+                onesCounter =0;
             }
-            count++;
         }
-        result.append("-"+list.get(list.size()-1));
-        return String.valueOf(result);
+
+        return Math.max(maxOnesLength,onesCounter);
     }
 
-
     public static void main(String[] args) {
-
-        Set<Integer> set = new HashSet<>(Arrays.asList(2, 3, 4, 6, 8, 9, 10));
-
-        System.out.println(setToGroup(set));
+        int[] nums = {0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1};
+        System.out.println(maxOnesLength(nums)); // Output should be 4
     }
 }
