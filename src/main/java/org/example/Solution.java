7f796d5be2c62bb55961f87e07a5b1f32344c13c
SGoriainov <wishmast777@gmail.com>
2024-05-01 06:36:10 +0400

    дубликаты. О(n)

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -7,44 +7,26 @@ import java.util.stream.IntStream;
 
 public class Solution {
 
-    public static int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) {
-            return new int[0][];
-        }
-        //обязательно сортируем отрезки
-        List<int[]> resultRanges = new ArrayList<>();
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-
-        int[] lastRange = null;
-
-        for (int[] rng : intervals) {
-            //если текущий отрезок не пересекается с последним отрезком
-            if (lastRange == null || rng[0] > lastRange[1]) {
-                resultRanges.add(rng);
-                lastRange = rng;
+    public static boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> maps= new HashMap<>();
+        for(int i=0; i< nums.length; i++) {
+            if(maps.containsKey(nums[i])) {
+                return true;
             } else {
-                //пересекатеся с последним отрезком, присваиваем новое значение концу отрезка
-                //значение в resultRanges автоматически обновляется, т.к ссылаются на одну ячейку памяти в массиве
-                lastRange[1] = Math.max(rng[1], lastRange[1]);
+                maps.put(nums[i],nums[i]);
             }
         }
-
-        return resultRanges.toArray(new int[resultRanges.size()][]);
+        return false;
     }
 
+
     public static void main(String[] args) {
-        int[][] intervals = {
-                {1, 3},
-                {2, 6},
-                {8, 10},
-                {9, 12},
-                {14, 17}
-        };
+        int[] intervals = new int[]{1,2,3,1};
 
-        int[][] mergedIntervals = merge(intervals);
 
-        for (int[] interval : mergedIntervals) {
-            System.out.println(Arrays.toString(interval));
-        }
+
+        containsDuplicate(intervals);
+
+
     }
 }
