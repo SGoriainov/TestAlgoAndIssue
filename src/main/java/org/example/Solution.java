e353212a044ab5ae51a8e4720549bf98d1bbdeaf
SGoriainov <wishmast777@gmail.com>
2024-05-15 12:22:38 +0400

    169. Majority Element. HashMap

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -7,25 +7,30 @@ import java.util.stream.IntStream;
 
 public class Solution {
 
-    public static boolean containsDuplicate(int[] nums) {
-        Map<Integer,Integer> maps= new HashMap<>();
-        for(int i=0; i< nums.length; i++) {
-            if(maps.containsKey(nums[i])) {
-                return true;
-            } else {
-                maps.put(nums[i],nums[i]);
+    public static int majorityElement(int[] nums) {
+        Map <Integer,Integer> groups = new HashMap<>();
+        int max = 0;
+        int res = 0;
+        for(int num : nums) {
+            if(!groups.containsKey(num)) {
+                groups.put(num, 0);
+
+            }
+            groups.put(num,groups.get(num)+1);
+
+            if(groups.get(num) > max) {
+                max = groups.get(num);
+                res = num;
             }
         }
-        return false;
+        return res;
     }
 
 
     public static void main(String[] args) {
-        int[] intervals = new int[]{1,2,3,1};
+        int[] intervals = new int[]{5,1,1,1,2,2,3,3,3,3};
 
-
-
-        containsDuplicate(intervals);
+        System.out.println(majorityElement(intervals));
 
 
     }
