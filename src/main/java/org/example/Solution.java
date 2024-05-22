6b45561ba4bc92eff55496a3fc7826c492f58484
SGoriainov <wishmast777@gmail.com>
2024-05-22 19:56:34 +0400

    149. Max Points on a Line

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -5,26 +5,59 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 
-public class Solution {
+public class Solution{
+    public static int maxPoints(Point[] points) {
+        if (points==null) return 0;
+        if (points.length<=2) return points.length;
 
-    public static boolean isAnagram(String s, String t) {
-        //Map<String,String> stringMap = new HashMap<>();
-        char[] charS = s.toCharArray();
-        char[] charT = t.toCharArray();
-        Arrays.sort(charS);
-        Arrays.sort(charT);
-        String Ss= new String(charS);
-        String Tt= new String(charT);
-        if(Ss.equals(Tt)) return true;
-        return false;
+        Map<Integer,Map<Integer,Integer>> map = new HashMap<Integer,Map<Integer,Integer>>();
+        int result=0;
+        for (int i=0;i<points.length;i++){
+            map.clear();
+            int overlap=0,max=0;
+            for (int j=i+1;j<points.length;j++){
+                int x=points[j].x-points[i].x;
+                int y=points[j].y-points[i].y;
+                if (x==0&&y==0){
+                    overlap++;
+                    continue;
+                }
+                int gcd=generateGCD(x,y);
+                if (gcd!=0){
+                    x/=gcd;
+                    y/=gcd;
+                }
+
+                if (map.containsKey(x)){
+                    if (map.get(x).containsKey(y)){
+                        map.get(x).put(y, map.get(x).get(y)+1);
+                    }else{
+                        map.get(x).put(y, 1);
+                    }
+                }else{
+                    Map<Integer,Integer> m = new HashMap<Integer,Integer>();
+                    m.put(y, 1);
+                    map.put(x, m);
+                }
+                max=Math.max(max, map.get(x).get(y));
+            }
+            result=Math.max(result, max+overlap+1);
+        }
+        return result;
+
+
     }
+    private int generateGCD(int a,int b){
+
+        if (b==0) return a;
+        else return generateGCD(b,a%b);
+
+    }
+}
 
 
     public static void main(String[] args) {
-        //int[] intervals = new int[]{5,1,1,1,2,2,3,3,3,3};
-        String s = "anagram";
-        String t = "nagaram";
-        System.out.println(isAnagram(s,t));
+
 
 
     }
